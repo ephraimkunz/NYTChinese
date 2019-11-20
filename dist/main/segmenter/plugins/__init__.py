a0c9d46f10f3610786903931d5dc2c63e4583558
Ephraim Kunz <ephraimkunz@me.com>
2019-11-19 21:10:26 -0800

    Don't commit dist or build dirs

--- dist/main/segmenter/plugins/__init__.py
+++
@@ -1,7 +0,0 @@
-class SegmentMethodPlugin(object):
-    key = "UNIMPLEMENTED"
-    name = "Base class for Segment Methods"
-    description = "Base class for Segment Methods"
-
-    def segment(self, segmenter, text, updatefunction=None):
-        pass
